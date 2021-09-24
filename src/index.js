3725e621fe1b529a927a36f3821e371866cdf349
jhollier <jhollier@blueorigin.com>
2021-09-23 21:41:45 -0700

    removed yarn.lock

--- src/index.js
+++ src/index.js
@@ -4,5 +4,11 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+// app.listen(port);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
